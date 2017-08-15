7093364e2dbcd5924b6ce084de330d3b85169d6c
Tarik Curto <centro.tarik@live.com>
2017-08-15 08:17:27 +0200

    + Drupal controller  + Drupal config link  + Drupal module file

---
+++ src/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\ner_importer;
+
+use \Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Default controller of module
+ *
+ * @package Drupal\ner
+ */
+class MainController extends ControllerBase{
+
+    /**
+     * NER importer home page.
+     *
+     * @return array
+     */
+    public function setup(){
+
+        return [
+
+        ];
+    }
+}
